8c93c4014e3ef349cd75de6d57d514104425660e
Munir Nassar <nassarmu@umn.edu>
2026-05-08 19:06:59 -0500

    add check for MANPATH existence before appending

--- packaging/resources/files/puppet-agent.sh
+++ packaging/resources/files/puppet-agent.sh
@@ -6,6 +6,13 @@ elif ! echo "${PATH}" | grep -q /opt/puppetlabs/bin ; then
   export PATH="${PATH}:/opt/puppetlabs/bin"
 fi
 
-if ! echo "${MANPATH-}" | grep -q /opt/puppetlabs/puppet/share/man ; then
-  export MANPATH="${MANPATH-}:/opt/puppetlabs/puppet/share/man"
+if [ -z ${MANPATH-} ]
+then
+  export MANPATH=/opt/puppetlabs/puppet/share/man
+else
+  # if MANPATH is set, but does not have our mandir, append our mandir
+  if ! echo "${MANPATH-}" | grep -q /opt/puppetlabs/puppet/share/man
+  then
+    export MANPATH="${MANPATH-}:/opt/puppetlabs/puppet/share/man"
+  fi
 fi
